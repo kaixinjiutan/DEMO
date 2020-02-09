37271eb50c5615bd02ae38da50b399320b60f971
weiyan <309701210@qq.com>
2020-02-09 16:01:06 +0900

    init
    init

---
+++ quasar-todo/src/pages/Help.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      You are, my fire, The one, desire Believe, when I say, I want it that way
+      But we, are two worlds apart, Can't reach to your heart, When you say,
+      That I want it that way Tell me why, Ain't nothin' but a heartache, Tell
+      me why,
+    </p>
+    <p>
+      Ain't nothin' but a mistake, Tell me why, I never wanna hear you
+      say, I want it that way Am I, your fire? Your one, desire, desire, it's
+      too late, But I want it that way Tell me why, Ain't nothin' but a
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
